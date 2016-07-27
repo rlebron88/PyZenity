2c462c0f6ad7bc205e68d253cbd45af89322c839
Ricardo Lebrón <rlebron88@gmail.com>
2016-07-27 16:18:05 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='PyZenity',
+      version='0.1.7',
+      description='An easy to use frontend to Zenity',
+      author='Brian Ramos',
+      author_email='bramos@brianramos.com',
+      maintainer='Brian Ramos',
+      maintainer_email='bramos@brianramos.com',
+      url='http://www.brianramos.com/?page_id=110',
+      download_url='http://www.brianramos.com/software/PyZenity/PyZenity-0.1.7.tar.gz',
+      license='MIT',
+      long_description='PyZenity is an easy to use interface to Zenity for Python.  Zenity is normally called from scripts by invoking it with a multitude of command line parameters that it uses to construct its interfaces.  This module hides the details of invoking the command and presents simple API functions.',
+      py_modules=['PyZenity'])
